03327bd34a84916c2e63dbf58a39a350564c7614
Aleksander Angelov <Obelixx@users.noreply.github.com>
2015-01-20 02:51:13 +0200

    PrimitiveDataTypesAndVariables-Homework
    Homework - Full

---
+++ PrimitiveDataTypesAndVariables-Homework/13.Comparing_Floats/Comparing_Floats.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+//Problem 13.* Comparing Floats
+
+//    Write a program that safely compares floating-point numbers (double) with precision eps = 0.000001.
+
+//Note: Two floating-point numbers a and b cannot be compared directly by a == b because of the nature of the floating-point arithmetic. Therefore, we assume two numbers are equal if they are more closely to each other than a fixed constant eps.
+
+//Examples:
+//Number a 	Number b 	Equal (with precision eps=0.000001) 	Explanation
+//5.3 	6.01 	false 	The difference of 0.71 is too big (> eps)
+//5.00000001 	5.00000003 	true 	The difference 0.00000002 < eps
+//5.00000005 	5.00000001 	true 	The difference 0.00000004 < eps
+//-0.0000007 	0.00000007 	true 	The difference 0.00000077 < eps
+//-4.999999 	-4.999998 	false 	Border case. The difference 0.000001 == eps. We consider the numbers are different.
+//4.999999 	4.999998 	false 	Border case. The difference 0.000001 == eps. We consider the numbers are different.
+
+class Comparing_Floats
+{
+    static void Main()
+    {
+        double a;
+        double b;
+        double eps = 0.000001;
+
+        Console.WriteLine("Precioson is:  {0,12}\nin other words:{1,12}\n", eps, eps.ToString("0.000000"));
+
+        a = 5.3d;//	false
+        b = 6.01d;
+        Console.WriteLine("{0,12} == {1,12} is {2,12}  - {3,5}"
+            , a
+            , b
+            , (a - b < 0) ? (b - a < eps) : (a - b < eps)
+            , ((a - b < 0) ? (b - a < eps) == false : (a - b < eps) == false) ? "OK!" : "Not OK :/");
+
+        a = 5.00000001d; //true
+        b = 5.00000003d;
+        Console.WriteLine("{0,12} == {1,12} is {2,12}  - {3,5}"
+            , a
+            , b
+            , (a - b < 0) ? (b - a < eps) : (a - b < eps)
+            , ((a - b < 0) ? (b - a < eps) == true : (a - b < eps) == true) ? "OK!" : "Not OK :/");
+
+        a = 5.00000005d; //true
+        b = 5.00000001d;
+        Console.WriteLine("{0,12} == {1,12} is {2,12}  - {3,5}"
+            , a
+            , b
+            , (a - b < 0) ? (b - a < eps) : (a - b < eps)
+            , ((a - b < 0) ? (b - a < eps) == true : (a - b < eps) == true) ? "OK!" : "Not OK :/");
+
+        a = -0.0000007d; //true
+        b = 0.00000007d;
+        Console.WriteLine("{0,12} == {1,12} is {2,12}  - {3,5}"
+            , a
+            , b
+            , (a - b < 0) ? (b - a < eps) : (a - b < eps)
+            , ((a - b < 0) ? (b - a < eps) == true : (a - b < eps) == true) ? "OK!" : "Not OK :/");
+
+        a = -4.999999d; //false
+        b = -4.999998d;
+        Console.WriteLine("{0,12} == {1,12} is {2,12}  - {3,5}"
+            , a
+            , b
+            , (a - b < 0) ? (b - a < eps) : (a - b < eps)
+            , ((a - b < 0) ? (b - a < eps) == false : (a - b < eps) == false) ? "OK!" : "Not OK :/");
+        a = 4.999999d; //false
+        b = 4.999998d;
+        Console.WriteLine("{0,12} == {1,12} is {2,12}  - {3,5}"
+            , a
+            , b
+            , (a - b < 0) ? (b - a < eps) : (a - b < eps)
+            , ((a - b < 0) ? (b - a < eps) == false : (a - b < eps) == false) ? "OK!" : "Not OK :/");
+
+    }
+}
+
---
+++ PrimitiveDataTypesAndVariables-Homework/14.Print_the_ASCII_Table/Print_the_ASCII_Table.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+//Problem 14.* Print the ASCII Table
+
+//    Find online more information about ASCII (American Standard Code for Information Interchange) and write a program that prints the entire ASCII table of characters on the console (characters from 0 to 255).
+
+//Note: Some characters have a special purpose and will not be displayed as expected. You may skip them or display them differently.
+
+//Note: You may need to use for-loops (learn in Internet how).
+
+class Print_the_ASCII_Table
+{
+    static void Main()
+    {
+        System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.WriteLine("CC means Control Character\n");
+
+        Console.WriteLine("ASCII Tabble (16 in row):");
+        for (int i = 0; i <= 255; i++)
+        {
+            if (!Char.IsControl(Convert.ToChar(i)))
+            {
+                Console.Write("[{0}] ", Convert.ToChar(i));
+            }
+            else Console.Write("[CC]");
+            if ((i+1)% 16 == 0) Console.Write("\n");
+        }
+    }
+}
+
