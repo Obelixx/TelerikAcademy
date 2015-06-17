33427c6779588e8a5f5c3bf92bc9bf8fb910ea72
Aleksander Angelov <Obelixx@users.noreply.github.com>
2015-06-17 11:27:08 +0300

    JavaScript-OOP
    first homework:
    1.Functions_and_FunctionExpressions

---
+++ JavaScript/JavaScript-OOP/1.Functions_and_FunctionExpressions/Functions_and_FunctionExpressions/task-1.js
@@ -0,0 +1,27 @@
+﻿/* Task Description */
+/* 
+	Write a function that sums an array of numbers:
+		numbers must be always of type Number
+		returns `null` if the array is empty
+		throws Error if the parameter is not passed (undefined)
+		throws if any of the elements is not convertible to Number	
+*/
+
+function solve() {
+    return function sum(arr) {
+        if (arguments.length === 0) { throw new Error('the parameter is not passed'); }
+        if (!arr.length) { return null; }
+        var tempSum = 0, len, i;
+        for (i = 0, len = arr.length; i < len; i += 1) {
+            if (!(+arr[i])) { throw new Error('any of the elements is not convertible to Number'); }
+            tempSum += +arr[i];
+        }
+        return (tempSum);
+    }
+    module.exports = sum;
+}
+
+arr = [1,2,'3']
+sum = solve(arr);
+console.log('arr = ' + arr.join(', '));
+console.log('Prime numbers = ' + sum(arr));
---
+++ JavaScript/JavaScript-OOP/1.Functions_and_FunctionExpressions/Functions_and_FunctionExpressions/task-2.js
@@ -0,0 +1,43 @@
+﻿//Task 2.
+
+//Write a function that finds all the prime numbers in a range
+//    It should return the prime numbers in an array
+//    It must throw an Error if any of the range params is not convertible to Number
+//    It must throw an Error if any of the range params is missing
+
+function solve() {
+    return function PrimesInRange(start, end) {
+        var i, arrOfPrimes = [];
+        if (start === undefined || end === undefined) { throw new Error('any of the range params is missing') }
+        if (!(+start || +end)) { throw new Error('any of the range params is not convertible to Number') }
+        start = +start;
+        end = +end;
+        if (start > end) {
+            start = start + end;
+            end = start - end;
+            start = start + end
+        }
+        for (i = start; i <= end; i += 1) {
+            if (isPrime(i)) {
+                arrOfPrimes.push(i);
+            }
+        }
+        return arrOfPrimes;
+
+        function isPrime(num) {
+            var i;
+            for (i = 2; i < num; i++) {
+                if (!(num % i)) break;
+            }
+            return (i === num);
+        }
+    }
+    module.exports = PrimesInRange;
+}
+
+
+var start = 0;
+var end = 5;
+PrimesInRange = solve(start, end);
+console.log('start = ' + start + ' end = ' + end);
+console.log('Prime numbers = ' + PrimesInRange(start, end));
